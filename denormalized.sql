3d9d6315ee9e4ab455eb4108c19a958cce7a236e
Aleksey Romanov <a_romanov@wargaming.net>
2019-05-09 08:44:31 +0300

    Add schema

--- denormalized.sql
+++ denormalized.sql
@@ -1,8 +1,9 @@
---DROP TABLE IF EXISTS player;
-CREATE TABLE IF NOT EXISTS player
+DROP TABLE IF EXISTS player;
+CREATE TABLE IF NOT EXISTS denormalized.player
 (
     id BIGINT NOT NULL,
     nickname TEXT NOT NULL,
+    email TEXT NOT NULL,
     password TEXT NOT NULL,
     dead_crystal_count INTEGER NOT NULL,
     game_crystal_count INTEGER NOT NULL,
@@ -20,7 +21,7 @@ CREATE TABLE IF NOT EXISTS player
 
 
 --DROP TABLE IF EXISTS log_action;
-CREATE TABLE IF NOT EXISTS log_action 
+CREATE TABLE IF NOT EXISTS denormalized.log_action
 (
     id BIGINT NOT NULL,
     player_id BIGINT NOT NULL,
@@ -33,7 +34,7 @@ CREATE TABLE IF NOT EXISTS log_action
     is_achievement1 BOOLEAN,
     is_achievement2 BOOLEAN,
     created timestamp without time zone DEFAULT timezone('UTC'::text, now()),
-    
+
     CONSTRAINT log_action_id_pkey PRIMARY KEY (id)
 );
-CREATE INDEX IF NOT EXISTS log_action_player_idx ON log_action (player_id);
+CREATE INDEX IF NOT EXISTS log_action_player_idx ON denormalized.log_action (player_id);
